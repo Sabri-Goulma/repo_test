362799a90e01d6dd95d5c7f9e2466000979dfea4
m-goulma <m.goulma@instadeep.com>
2025-10-14 11:34:33 +0200

    add all files

---
+++ smoke-test/src/test_pvc2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 " > /mnt/testprojpvc-af7b62643072486e-andanotheron-19b831fed72c429e/file2.txt
+#cat /mnt/import-66eccab3d9c1483d-premium-test-b1671a25c25d4d7d/file.txt
+#cat /mnt/testprojpvc-af7b62643072486e-andanotheron-19b831fed72c429e/file2.txt
+sleep 100
+echo "sleeping 100"
---
+++ test_pvc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 " > /mnt/testprojpvc-af7b62643072486e-pvcnonexpand-b3799fecd7264c8d/file2.txt
+#cat /mnt/import-66eccab3d9c1483d-premium-test-b1671a25c25d4d7d/file.txt
+#cat /mnt/testprojpvc-af7b62643072486e-pvcnonexpand-b3799fecd7264c8d/file2.txt
+sleep 100
+echo "sleeping 100"
